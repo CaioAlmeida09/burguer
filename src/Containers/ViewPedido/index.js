9c7c744b4bae36129d4cf7c5297bd7eada9f0b5c
CaioAlmeida09 <Caio09092001@gmail.com>
2023-07-09 09:36:59 -0300

    navegando entre as telas da aplicação

--- src/Containers/ViewPedido/index.js
+++ src/Containers/ViewPedido/index.js
@@ -3,18 +3,20 @@ import {Background, Input} from "../ViewPedido/style"
 import ImgPedido from '../../assets/img-saco.png'
 import Button from '../../components/button/index'
 import Title from '../../components/title/index'
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 function AppView(){
-
-}
+    const Navigate = useNavigate();
+    function BackCreate(){
+        Navigate("/")}
+      
 return(
     <Background>
         <img src={ImgPedido} ></img>
         <Title>Pedidos</Title>
         <Input/>
         <Input/>
-        <Button> voltar </Button>
+        <Button onClick={BackCreate}> voltar </Button>
         
     </Background>
 )
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,13 +1,12 @@
 import React from "react"
 import Button from "./style"
-import { useNavigate } from "react-router-dom";
 
-const navigate = useNavigate();
-function BackCreate(){
-  navigate("/")}
-function MyButton(){
+function MyButton({children, ...props}){
 
-return( <Button onClick={BackCreate}> voltar </Button>)
+
+return( 
+
+<Button {...props}> {children} </Button>)
+
 }
-
 export default MyButton
