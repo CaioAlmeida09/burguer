4fb7cf67ea7ec2e60f62a83d553d27f88ef04079
CaioAlmeida09 <Caio09092001@gmail.com>
2023-07-16 10:56:40 -0300

    dando vida aos Inputs utilizando o useRef

--- src/Containers/CreatePedido/index.js
+++ src/Containers/CreatePedido/index.js
@@ -1,29 +1,38 @@
-import  React from "react"
-import { Background, Label, Input} from "./style"
-import ImgBurguer from '../../assets/img-burguer.png'
-import { useNavigate } from "react-router-dom"
-import Title from "../../components/title/index"
-import Button from "../../components/button"
+import  React from "react";
+import { Background, Label, Input, ButtonPedido} from "./style";
+import ImgBurguer from '../../assets/img-burguer.png';
+import { useNavigate } from "react-router-dom";
+import Title from "../../components/title/index";
+import Button from "../../components/button";
+import {useState, useRef} from  "react"
 
 function AppHome(){
     const navigate = useNavigate();
+    const [pedido, setPedido ] = useState()
+    const InputPedido = useRef()
+    const InputCliente = useRef()
 
+
+    function AddPedido(){
+        console.log(InputPedido.current.value)
+        console.log(InputCliente.current.value)}
+        
     function RouterView(){
         navigate("/View")
     }
 
-return(
+return (    
     <Background>
         <img src={ImgBurguer} ></img>
         <Title>Faça seu Pedido</Title>
         <Label> Pedido: </Label>
-        <Input/>
+        <Input ref={InputPedido}/>
         <Label > Nome do cliente:</Label>
-        <Input/>
+        <Input ref={InputCliente}/>
         <Button ButtonCreate = {true} onClick={RouterView}> cadastrar </Button>
+        <ButtonPedido onClick={AddPedido}> jjhsjj </ButtonPedido>
         
     </Background>
 )
-
 }
 export default AppHome
